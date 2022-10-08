c91d76000623e3ba711a5ab9ec7d3e8cba6afb94
BoMeneerNL <51164986+BoMeneerNL@users.noreply.github.com>
2022-10-08 17:41:25 +0200

    Added route counter

--- scripts/startup.js
+++ scripts/startup.js
@@ -1,8 +1,13 @@
-mapboxgl.accessToken = "pk.eyJ1IjoiYm9tZW5lZXJubCIsImEiOiJjbDhzbnlmY2wwODBjM25zeTJsazhtZGV3In0.PiO2nPF0qeoOxgO41QxmTw";
+mapboxgl.accessToken = "%Token%";
+//valuetype = meter
 var afstand = 0;
+//valuetype = seconds
 var tijd = 0;
 var currentafstand = 0;
 var currentprice = 0;
+var currenttijd;
+// m per sec
+var uppersecond;
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/streets-v11",
@@ -14,34 +19,51 @@ const route = new MapboxDirections({
   profile: 'mapbox/driving',
   startLocation: '5.725, 51.80917',
   unit: 'metric',
-  language: 'nl-NL',
+  language: 'nl-NL'
 });
 
 map.addControl(route, 'top-left');
-
 route.on("route", (e) => {
   afstand = e.route[0].distance;
+  tijd = e.route[0].duration;
   document.getElementById("pricefullride").innerText = "Prijs geselecteerde route: " + CalculateCost(afstand).toFixed(2)+ "€";
-  document.getElementById("distancefullride").innerText = "Afstand geselecteerde route: " + (afstand/1000).toFixed(2) + "km";
+  document.getElementById("distancefullride").innerText = "Afstand geselecteerde route: " + (afstand >= 1000?(afstand/1000).toFixed(2) + "km": afstand.toFixed(0) + "m");
 
 })
 document.getElementById("mapbox-directions-profile-driving").remove();
 document.getElementById("mapbox-directions-profile-walking").remove();
 document.getElementById("mapbox-directions-profile-cycling").remove();
 var active_route = false;
+var contradict;
 function StartStopRoute(){
   if(!active_route){
     document.getElementById("endride").style.display = "block";
     document.getElementById("startride").style.display = "none";
     active_route = !active_route;
-    setTimeout()
+    uppersecond = (afstand) / 20;
+    contradict = setInterval(RideEditor,100);
   }
   else if(active_route){
     document.getElementById("endride").style.display = "none";
     document.getElementById("startride").style.display = "block";
+    clearInterval(contradict);
     active_route = !active_route;
   }
 }
-function CalculateCost(distance){
-  return (distance * 0.00113) + 3.25
+//0,00113 = 1,13€ per km, 3,25€ = starttarief
+const CalculateCost = (distance) => (distance * 0.00113) + 3.25;
+function RideEditor(){
+  if(currenttijd >= tijd){
+    clearInterval(contradict);
+  }
+  else{
+    currenttijd++;
+    currentafstand += uppersecond;
+    currentprice = CalculateCost(currentafstand);
+    document.getElementById("priceridden").innerText = "Prijs: " + currentprice.toFixed(2) + "€";
+    document.getElementById("ridden").innerText = "Afstand: " + (currentafstand >= 1000?(currentafstand/1000).toFixed(2) + "km": currentafstand.toFixed(0) + "m");
+  }
+  currenttijd += 100;
+
+
 }
